8bc7de568419037ac6466408a446f1340c9e8081
Tatiana Zemliakova <tatzemliakova@gmail.com>
2024-09-20 11:33:53 +0200

    Animation Apropos

--- src/components/Apropos/index.jsx
+++ src/components/Apropos/index.jsx
@@ -23,41 +23,53 @@ function Apropos() {
   }
   return (
     <div className="home">
-      <Fade left>
-        <div className="blocApropos">
-          <div className="blocTitre">
-            <div className="blocTitre__titre">À propos</div>
-            <div className="blocTitre__bloc"></div>
+      <motion.div
+        className="blocApropos"
+        initial={{ opacity: 0, x: -600 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{
+          ease: 'easeInOut',
+          type: 'spring',
+          duration: 1.5,
+        }}
+        whileHover={{ scale: 1.02 }}
+        whileTap={{ scale: 0.98 }}
+      >
+        <div className="blocTitre">
+          <div className="blocTitre__titre">À propos</div>
+          <div className="blocTitre__bloc"></div>
+        </div>
+        <div className="blocBleu">
+          <div className="blocBleu__image">
+            <div className="blocBleu__image__ellipseBlanche">
+              <div className="blocBleu__image__ellipseRose"></div>
+            </div>
+            <img src={photo} alt="Tatiana" className="blocBleu__image__photo" />
           </div>
-          <div className="blocBleu">
-            <div className="blocBleu__image">
-              <div className="blocBleu__image__ellipseBlanche">
-                <div className="blocBleu__image__ellipseRose"></div>
-              </div>
-              <img
-                src={photo}
-                alt="Tatiana"
-                className="blocBleu__image__photo"
+          <div className="blocBleu__contenu">
+            <div className="blocBleu__contenu__text">
+              <p>Je suis</p>
+              <motion.img
+                src={WebDev}
+                alt="Web Developpeur"
+                className="blocBleu__contenu__text__dev"
+                animate={{ rotate: 360 }}
+                transition={{
+                  repeat: Infinity,
+                  ease: 'linear',
+                  duration: 4,
+                }}
               />
             </div>
-            <div className="blocBleu__contenu">
-              <div className="blocBleu__contenu__text">
-                <p>Je suis</p>
-                <img
-                  src={WebDev}
-                  alt="Web Developpeur"
-                  className="blocBleu__contenu__text__dev"
-                />
-              </div>
-              <p className="blocBleu__contenu__nom">
-                Tatiana
-                <br />
-                Youssoufa Saidou
-              </p>
-            </div>
+            <p className="blocBleu__contenu__nom">
+              Tatiana
+              <br />
+              Youssoufa Saidou
+            </p>
           </div>
         </div>
-      </Fade>
+      </motion.div>
+
       <div className="blocMenu">
         <h1 className="blocMenu__titre">Portfolio˺</h1>
         <div className="blocMenu__contenu1">
@@ -65,32 +77,47 @@ function Apropos() {
             <img src={ordinateur} alt="La conception WEB" />
           </div>
 
-          <div className="blocMenu__contenu1__blocs">
-            <Fade top delay={500}>
-              <div className="blocMenu__contenu1__blocs__gris">CV</div>
-            </Fade>
-            <Fade right delay={400}>
-              <a
-                className="blocMenu__contenu1__blocs__bleu"
-                href="#contact"
-                onClick={(e) => {
-                  e.preventDefault()
-                  handleScroll('contact')
-                }}
-              >
-                Contacts
-              </a>
-            </Fade>
-          </div>
+          <motion.div
+            className="blocMenu__contenu1__blocs"
+            initial={{ opacity: 0, x: 600 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{
+              ease: 'easeOut',
+              type: 'spring',
+              duration: 3.5,
+            }}
+          >
+            <motion.div
+              className="blocMenu__contenu1__blocs__gris"
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
+            >
+              CV
+            </motion.div>
+
+            <motion.a
+              className="blocMenu__contenu1__blocs__bleu"
+              whileHover={{ scale: 1.02 }}
+              whileTap={{ scale: 0.98 }}
+              href="#contact"
+              onClick={(e) => {
+                e.preventDefault()
+                handleScroll('contact')
+              }}
+            >
+              Contacts
+            </motion.a>
+          </motion.div>
         </div>
 
         <div className="blocMenu__contenu2">
           <motion.a
-            initial={{ opacity: 0, y: 600 }}
+            initial={{ opacity: 0, y: 400 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
+              ease: 'easeOut',
               type: 'spring',
-              duration: 1.5,
+              duration: 2.0,
             }}
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
@@ -104,27 +131,56 @@ function Apropos() {
             Mes compétences
           </motion.a>
 
-          <Fade bottom delay={200}>
-            <div className="blocMenu__contenu2__blocNoir">
-              <img
-                className="blocMenu__contenu2__blocNoir__image"
-                src={weblink}
-                alt="Abstraction"
-              />
-            </div>
-          </Fade>
-          <Fade right delay={300}>
-            <a
-              className="blocMenu__contenu2__blocBeige"
+          <motion.div
+            className="blocMenu__contenu2__blocNoir"
+            initial={{ opacity: 0, y: 600 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              ease: 'easeInOut',
+              type: 'spring',
+              duration: 2.5,
+            }}
+            whileHover={{ scaleX: 1.02 }}
+            whileTap={{ scaleX: 0.98 }}
+          >
+            <motion.img
+              className="blocMenu__contenu2__blocNoir__image"
+              src={weblink}
+              alt="Abstraction"
+              initial={{ opacity: 0, x: 600 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{
+                ease: 'easeInOut',
+                type: 'spring',
+                duration: 5.5,
+              }}
+            />
+            <motion.a
+              className="blocMenu__contenu2__blocNoir__text"
               href="#projets"
               onClick={(e) => {
                 e.preventDefault()
                 handleScroll('projets')
               }}
             >
-              Projets
-            </a>
-          </Fade>
+              Mes projets
+            </motion.a>
+          </motion.div>
+          <motion.a
+            className="blocMenu__contenu2__blocBeige"
+            initial={{ opacity: 0, x: 600 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{
+              ease: 'easeInOut',
+              type: 'spring',
+              duration: 3.0,
+            }}
+            whileHover={{ scaleX: 0.9 }}
+            drag="x"
+            dragConstraints={{ left: -150, right: 0 }}
+          >
+            Projets
+          </motion.a>
         </div>
       </div>
     </div>
