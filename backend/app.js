f8b149ea174d9d411dc9cc3024b10f457681812b
Tatiana Zemliakova <tatzemliakova@gmail.com>
2024-09-28 09:37:10 +0200

    Suppresion de la base des donnees

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
 
 const projetRoutes = require('./routes/projet_routes');
 
+/*
 mongoose.connect("mongodb+srv://tatzemliakova:BgQsVONwry88jYAF@cluster0.fsw72.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     { useNewUrlParser: true,
       useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+    */
 
 const app = express();
 
@@ -19,81 +20,11 @@ app.use((req, res, next) => {
     next();
   });
 
-//app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/projet', projetRoutes);
 
-/*
-app.use('/api/projet', (req, res, next) => {
-    const projet = [
-        {
-            id: 'kjghfkdfkl',
-            title: 'Booki',
-            description: 'Une agence de voyage qui permette aux usagers de trouver des hébergements et des activités dans la ville de leur choix',
-            cover: 'https://www.agera.asso.fr/app/uploads/2020/08/Projet.jpg',
-            images: [
-                'https://www.omebatobo.fr/wp-content/uploads/agence-de-voyage.jpg',
-                'https://www.parvisdesgentils.fr/wp-content/uploads/2020/02/agence-de-voyage.jpg',
-                'https://www.autour-dumonde.fr/sx-content/uploads/cms/img-presentation-1.jpg',
-                'https://www.civitatis.com/blog/wp-content/uploads/2023/07/shutterstock_262168289-1920x1243.jpg',
-            ],
-            skills: 'HTML, CSS',
-            github: 'https://github.com/unamezzora/Projet1.git',
-            color: 'rose'
 
-        },
-        {
-            id: 'fh145kdfg4',
-            title: 'Sophie Bluel',
-            description: 'Un site portfolio d’une architecte d’intérieur Sophie Bluel',
-            cover: 'https://www.agera.asso.fr/app/uploads/2020/08/Projet.jpg',
-            images: [
-                'https://www.omebatobo.fr/wp-content/uploads/agence-de-voyage.jpg',
-                'https://www.parvisdesgentils.fr/wp-content/uploads/2020/02/agence-de-voyage.jpg',
-                'https://www.autour-dumonde.fr/sx-content/uploads/cms/img-presentation-1.jpg',
-                'https://www.civitatis.com/blog/wp-content/uploads/2023/07/shutterstock_262168289-1920x1243.jpg',
-            ],
-            skills: 'HTML, CSS, Java Script',
-            github: 'https://github.com/unamezzora/Projet3.git',
-            color: 'beige'
-
-        },
-        {
-            id: 'hy987dft86',
-            title: 'Kasa',
-            description: 'Une entreprise de location d’appartements entre particuliers',
-            cover: 'https://www.agera.asso.fr/app/uploads/2020/08/Projet.jpg',
-            images: [
-                'https://www.omebatobo.fr/wp-content/uploads/agence-de-voyage.jpg',
-                'https://www.parvisdesgentils.fr/wp-content/uploads/2020/02/agence-de-voyage.jpg',
-                'https://www.autour-dumonde.fr/sx-content/uploads/cms/img-presentation-1.jpg',
-                'https://www.civitatis.com/blog/wp-content/uploads/2023/07/shutterstock_262168289-1920x1243.jpg',
-            ],
-            skills: 'React, Sass',
-            github: 'https://github.com/unamezzora/Projet5.git',
-            color: 'beige'
-
-        },
-        {
-            id: 'yz635pma37',
-            title: 'Mon Vieux Grimoire',
-            description: 'Un site de référencement et de notation de livres',
-            cover: 'https://www.agera.asso.fr/app/uploads/2020/08/Projet.jpg',
-            images: [
-                'https://www.omebatobo.fr/wp-content/uploads/agence-de-voyage.jpg',
-                'https://www.parvisdesgentils.fr/wp-content/uploads/2020/02/agence-de-voyage.jpg',
-                'https://www.autour-dumonde.fr/sx-content/uploads/cms/img-presentation-1.jpg',
-                'https://www.civitatis.com/blog/wp-content/uploads/2023/07/shutterstock_262168289-1920x1243.jpg',
-            ],
-            skills: 'Node.js, Express, MongoDB',
-            github: 'https://github.com/unamezzora/Projet6MVG.git',
-            color: 'bleu'
-        },
-    ];
-    res.status(200).json(projet);
-});
-*/
 
 module.exports = app;
--- backend/controllers/projet_ctrl.js
+++ backend/controllers/projet_ctrl.js
@@ -1,13 +1,28 @@
-const Projet = require('../models/Projet');
+const fs = require('fs');
+const path = require('path');
+
+const dataPath = path.join(__dirname, '../data.json');
 
 exports.getAllProjet = (req, res, next) => {
-    Projet.find()
-    .then(projets => res.status(200).json(projets))
-    .catch(error => res.status(400).json({ error }));
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Échec de la lecture des données'});
+        } 
+        const projets = JSON.parse(data);
+        res.status(200).json(projets);
+    });
 };
 
 exports.getOneProjet = (req, res, next) => {
-    Projet.findOne({ _id: req.params.id })
-    .then(projet => res.status(200).json(projet))
-    .catch(error => res.status(404).json({ error}));
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Échec de la lecture des données'});
+        }
+        const projets = JSON.parse(data);
+        const projet = projets.find((p) => p._id === req.params.id);
+        if (!projet) {
+            res.status(404).json({ error: 'Projet non trouvé'});
+        }
+        res.status(200).json(projet);
+    });
 };
